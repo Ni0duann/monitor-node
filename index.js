7a965686ff4c4a22658e4525c169af39539c5074
Ni0duann <2014567981@qq.com>
2025-02-07 17:37:48 +0800

    feat：添加 ESLint 和 Prettier 配置，集成 koa-bodyparser 中间件

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const Router = require('koa-router');
 const app = new Koa();
 const router = new Router();
+
+// 使用 bodyParser 中间件
+app.use(bodyParser());
 
 // 路由
 router.get('/', async ctx => {
@@ -10,6 +14,13 @@ router.get('/', async ctx => {
 
 router.get('/about', async ctx => {
   ctx.body = 'This is a simple Koa.js application.';
+});
+
+router.post('/api/user', async ctx => {
+  const { name } = ctx.request.body;
+  ctx.body = {
+    name
+  };
 });
 
 // 使用路由中间件
