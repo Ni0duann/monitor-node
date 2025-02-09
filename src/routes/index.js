e429907947298bff0da8ebce2733312c1cff1109
李荣熔 <13443892+a154488546@user.noreply.gitee.com>
2025-02-09 23:50:55 +0800

    feat: 创建路由聚合index.js

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const Router = require('koa-router');
+const performanceRoutes = require('./performance');
+const pageViewRoutes = require('./pageView');
+const pvuvRoutes = require('./pvuv');
+const durationRoutes = require('./duration');
+
+const router = new Router();
+
+// 聚合所有路由
+router.use(performanceRoutes.routes());
+router.use(pageViewRoutes.routes());
+router.use(pvuvRoutes.routes());
+router.use(durationRoutes.routes());
+
+module.exports = router;
