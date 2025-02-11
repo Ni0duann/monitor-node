c68fdc6fb4fa99337d7579678a68fab673d93d49
李荣熔 <13443892+a154488546@user.noreply.gitee.com>
2025-02-11 23:55:05 +0800

    feat:添加白屏路由到总路由

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,16 @@
 const Router = require('koa-router');
+
 const performanceRoutes = require('./performance');
-// const pageViewRoutes = require('./pageView');
 const pvuvRoutes = require('./pvuv');
 const durationRoutes = require('./duration');
+const whiteScreenRoutes = require('./whiteScreen');
 
 const router = new Router();
 
 // 聚合所有路由
 router.use(performanceRoutes.routes());
-// router.use(pageViewRoutes.routes());
 router.use(pvuvRoutes.routes());
 router.use(durationRoutes.routes());
+router.use(whiteScreenRoutes.routes());
 
 module.exports = router;
