d7dd9674f9372c3ff97f6c060cf4208541475e40
李荣熔 <13443892+a154488546@user.noreply.gitee.com>
2025-02-11 23:27:22 +0800

    feat:修改性能数据获取

--- src/utils/perfTransform.js
+++ src/utils/perfTransform.js
@@ -20,18 +20,18 @@ function transformData(data, userInfo) {
 
     points.push(perfPoint);
 
-    if (data.performance.whiteScreenCount !== undefined) {
-        const whiteScreenPoint = new Point('performanceData')
-            .timestamp(timestamp)
-            .tag('type', 'white_screen')
-            .tag('ip', commonTags.ip)
-            .tag('uuid', commonTags.uuid)
-            .tag('browser', commonTags.browser)
-            .tag('os', commonTags.os)
-            .tag('device_type', commonTags.deviceType)
-            .intField('count', data.performance.whiteScreenCount);
-        points.push(whiteScreenPoint);
-    }
+    // if (data.performance.whiteScreenCount !== undefined) {
+    //     const whiteScreenPoint = new Point('performanceData')
+    //         .timestamp(timestamp)
+    //         .tag('type', 'white_screen')
+    //         .tag('ip', commonTags.ip)
+    //         .tag('uuid', commonTags.uuid)
+    //         .tag('browser', commonTags.browser)
+    //         .tag('os', commonTags.os)
+    //         .tag('device_type', commonTags.deviceType)
+    //         .intField('count', data.performance.whiteScreenCount);
+    //     points.push(whiteScreenPoint);
+    // }
 
     return points;
 }
