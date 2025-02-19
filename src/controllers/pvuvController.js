74fdca4dcfcf90c4769f2cdbc3a20957618b4fec
lirongrong <154488546@qq.com>
2025-02-19 21:10:37 +0800

    feat: 修改数据获取和显示

--- src/controllers/pvuvController.js
+++ src/controllers/pvuvController.js
@@ -43,6 +43,35 @@ class PvuvController {
                 ctx.body = { error: '参数错误！必须提供pagePath和有效的dataType。' };
                 return;
             }
+            //前端传入开始时间和结束时间
+            let startRange = null;
+            let endRange = null;
+            if (ctx.query.startTime && ctx.query.endTime) {
+                startRange = new Date(ctx.query.startTime);
+                endRange = new Date(ctx.query.endTime);
+                
+                // 检查时间是否有效
+                if (isNaN(startRange.getTime()) || isNaN(endRange.getTime())) {
+                    ctx.status = 400;
+                    ctx.body = { error: '开始时间或结束时间格式不正确！' };
+                    return;
+                }
+                // 检查开始时间是否早于结束时间
+                if (startRange >= endRange) {
+                    ctx.status = 400;
+                    ctx.body = { error: '开始时间必须早于结束时间！' };
+                    return;
+                }
+            } else {
+                // 如果没有传入开始时间和结束时间，使用默认的 rangeTime
+                if (isNaN(rangeTime) || rangeTime <= 0) {
+                    ctx.status = 400;
+                    ctx.body = { error: 'rangeTime参数必须是正整数！' };
+                    return;
+                }
+                startRange = `-${rangeTime}d`;
+            }
+
 
             // 构造基础过滤条件
             let filterConditions = `r._measurement == "flowData" `;
@@ -61,33 +90,55 @@ class PvuvController {
             let data
             let totalCount
             if (dataType === 'pv') {
-                query = `
-                    from(bucket: "monitor data")
-                        |> range(start: -${rangeTime}d)
-                        |> filter(fn: (r) => ${filterConditions})
-                        |> sum(column: "_value")
-                `;
+                //若有指定开始时间和结束时间
+                if (typeof startRange === 'string') {
+                    query = `
+                        from(bucket: "monitor data")
+                            |> range(start: ${startRange})
+                            |> filter(fn: (r) => ${filterConditions})
+                            |> sum(column: "_value")
+                    `;
+                } else {
+                    query = `
+                        from(bucket: "monitor data")
+                            |> range(start: ${startRange.toISOString()}, stop: ${endRange.toISOString()})
+                            |> filter(fn: (r) => ${filterConditions})
+                            |> sum(column: "_value")
+                    `;
+                }
                 data = await influxService.queryData(query);
                 totalCount = data.reduce((acc, curr) => acc + (curr._value || 0), 0);
-
             } else { // uv处理
-                query = `
-                    from(bucket: "monitor data")
-                        |> range(start: -${rangeTime}d)
-                        |> filter(fn: (r) => ${filterConditions})
-                        // 按照时间排序，确保最早的记录排在前面
-                        |> sort(columns: ["_time"], desc: false)
-                        // 取每个分组的第一条记录
-                        |> first()
-                        // 去重 IP 地址
-                        |> distinct(column: "ip")
-                        |> count()
-                `;
-              
+                if (typeof startRange === 'string') {
+                    query = `
+                        from(bucket: "monitor data")
+                            |> range(start: ${startRange})
+                            |> filter(fn: (r) => ${filterConditions})
+                            // 按照时间排序，确保最早的记录排在前面
+                            |> sort(columns: ["_time"], desc: false)
+                            // 取每个分组的第一条记录
+                            |> first()
+                            // 去重 IP 地址
+                            |> distinct(column: "ip")
+                            |> count()
+                    `;
+                } else {
+                    query = `
+                        from(bucket: "monitor data")
+                            |> range(start: ${startRange.toISOString()}, stop: ${endRange.toISOString()})
+                            |> filter(fn: (r) => ${filterConditions})
+                            // 按照时间排序，确保最早的记录排在前面
+                            |> sort(columns: ["_time"], desc: false)
+                            // 取每个分组的第一条记录
+                            |> first()
+                            // 去重 IP 地址
+                            |> distinct(column: "ip")
+                            |> count()
+                    `;
+                }
                 data = await influxService.queryData(query);
                 totalCount = data.length > 0 ? data[0]._value : 0;
             }
-
             // console.log('查询到的pvuvssssssss数据:', data); // 输出查询到的数据      
             ctx.body = { success: true, totalCount };
         } catch (err) {
